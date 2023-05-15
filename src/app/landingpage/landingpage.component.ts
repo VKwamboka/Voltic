9bf81eea898e3e62f0ac887f8f90bc45471a76c1
VKwamboka <verahnyagaka101@gmail.com>
2023-05-15 14:10:58 +0300

    added navbar

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule,RouterModule,FooterComponent],
+  imports: [CommonModule,RouterModule,FooterComponent,NavbarComponent],
   templateUrl: './landingpage.component.html',
   styleUrls: ['./landingpage.component.css']
 })
