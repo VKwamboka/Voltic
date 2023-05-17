c1de8b2a0a0beb10ca8d45f849dfb2f8f21d1354
VKwamboka <verahnyagaka101@gmail.com>
2023-05-17 08:24:25 +0300

    updated about us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'', loadComponent:()=>import('./landingpage/landingpage.component').then(l=>l.LandingpageComponent)},
   {path:'about', loadComponent:()=>import('./about/about.component').then(a=>a.AboutComponent)},
+  {path:'about/awesome-team',loadComponent:()=>import('./awesome-team/awesome-team.component').then(a=>a.AwesomeTeamComponent)},
+
 
 
 ];
--- src/app/awesome-team/awesome-team.component.ts
+++ src/app/awesome-team/awesome-team.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-awesome-team',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule,NavbarComponent,FooterComponent],
   templateUrl: './awesome-team.component.html',
   styleUrls: ['./awesome-team.component.css']
 })
